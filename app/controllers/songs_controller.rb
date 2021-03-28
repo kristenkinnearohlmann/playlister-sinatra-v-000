f5892c33461300b8ac9eb228dcc9d571ac14d8f0
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-28 18:53:56 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -56,7 +56,7 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     song = Song.find_by_slug(params[:slug])
-    binding.pry
+
     # Create or find artist
     if !params[:song][:artist].empty?
       artist = Artist.new(name: params[:song][:artist])
@@ -67,6 +67,7 @@ class SongsController < ApplicationController
       end
     end
 
+    song.artist = artist
     binding.pry
     redirect "/songs/#{song.slug}"
   end
